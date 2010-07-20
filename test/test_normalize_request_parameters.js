39161763d7684635e53dac92e6ae25b6a9d103f5
Stephan Zalewski <stephan@foo-labs.com>
2010-07-20 23:24:22 +0800

    added fillURL to upgrade a path to a full size url

---
+++ test/test_normalize_request_parameters.js
@@ -0,0 +1,23 @@
+var vows = require('vows'),
+    assert = require('assert');
+
+var oauth = require('oauth');
+
+var params_for_signature = function(params) {
+  return oauth.params_for_signature(params);
+};
+
+vows.describe('Normalize Request Parameters').addBatch({
+  'when normalizing an object': {
+    topic: function () {
+      return oauth.normalize(
+        {d: 1, b: 2, x: 3, a: 4, c: 5, func: function() { return } })
+      },
+    'it should be sorted lexiographically': function(topic) {
+      assert.deepEqual(topic, {a: 4, b: 2, c: 5, d: 1, x: 3});
+    },
+    'it should exclude functions': function(topic) {
+      assert.deepEqual(topic, {a: 4, b: 2, c: 5, d: 1, x: 3});
+    }
+  }
+}).export(module);
