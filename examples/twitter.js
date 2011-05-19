ed9140ee26510d4b001859b446f72e82bed3ed7f
Ryan Fairchild <ryan.fairchild@gmail.com>
2011-05-19 07:16:10 -0700

    Edited examples/twitter.js via GitHub

--- examples/twitter.js
+++ examples/twitter.js
@@ -3,8 +3,8 @@ var oauth = require('../lib/oauth'),
 		sys = require('sys'),
 		fs = require('fs');
 
-var consumerKey = 'Bwl6RuAsRdF6WNSdTIO2jw';
-var consumerSecret = 'ZlzjyRkAlkAaWf6Lrepby3YtmkvMHY3NK0DVqG7GFc';
+var consumerKey = '';
+var consumerSecret = '';
 
 var client = oauth.createClient(443,'api.twitter.com',true);
 
